fd17dc873098b07b327a3078ecc979905e24a446
gaston <glucero@gps.cl>
2016-03-01 12:52:59 -0300

    Clases de test y archoivp conf

---
+++ AkkaSamples/src/main/java/git/gaston/akka/samples/SampleActorLambda.java
@@ -0,0 +1,26 @@
+package git.gaston.akka.samples;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+
+/**
+ * Actor com lambdas, que recibe los mensajes que se le envian desde la clase main y los reenvia al logActor, que es
+ * creado en el constructor del mismo
+ *
+ * @author gaston
+ */
+public class SampleActorLambda extends AbstractActor {
+
+	private ActorRef logActor;
+
+	public SampleActorLambda() {
+		logActor = getContext().actorOf(Props.create(LogActor.class));
+		receive(ReceiveBuilder.				
+				match(Double.class, intMsg -> {
+					logActor.tell("DoubleMsg=" + intMsg, self());
+				}).build());
+	}
+
+}
